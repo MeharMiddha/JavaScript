a988ce4c97f6029bcda7b664b9669f3e51a8b04a
Mehar Middha <103279305+MeharMiddha@users.noreply.github.com>
2023-07-27 13:51:22 +0530

    Create script.js
    this is the scripting page for import and export in module

---
+++ Import, Export In Module/script.js
@@ -0,0 +1,7 @@
+// import { mul,add } from "./utility.js";
+
+import * as utility from './utility.js';
+
+const result = utility.mul(2,3,4);
+console.log(result);
+console.log(utility.STUDENTS_COUNT);
