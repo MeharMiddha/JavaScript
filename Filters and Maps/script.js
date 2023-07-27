238c7ca90f789078c9ac5968cb435b0fda9afb1b
Mehar Middha <103279305+MeharMiddha@users.noreply.github.com>
2023-07-27 13:22:36 +0530

    Create script.js
    it is the scripting page for filters and maps

---
+++ Filters and Maps/script.js
@@ -0,0 +1,27 @@
+let students = [
+    { id : "001", name : "Anish", sports : "Cricket" },
+    { id : "002", name : "Smriti", sports : "Basketball" },
+    { id : "003", name : "Rahul", sports : "Cricket" },
+    { id : "004", name : "Bakul", sports : "Basketball" },
+    { id : "005", name : "Nikita", sports : "Hockey" },
+]
+
+const newArray = students.filter((curValue) => {
+    if(curValue.id %2 ==0) return true;
+    else return false;
+})
+
+console.log("New Array here...");
+console.log(newArray);
+
+
+const names = 
+students.map((curValue, index, array) => {
+    return `<li>${curValue.name}</li>`;
+})
+
+console.log(names);
+
+const div = document.getElementById('container');
+
+div.innerHTML = `<ul>${names.join('')}</ul>`;
